c24946a7a6aba92233936dbeb27b2c8e703b72b8
duart38 <duartsnel@icloud.com>
2022-09-30 17:15:33 +0200

    new build
    Signed-off-by: duart38 <duartsnel@icloud.com>

--- dist/index.js
+++ dist/index.js
@@ -1,26 +1,3 @@
-/*
- *   Copyright (c) 2022 Duart Snel
- *   All rights reserved.
-
- *   Permission is hereby granted, free of charge, to any person obtaining a copy
- *   of this software and associated documentation files (the "Software"), to deal
- *   in the Software without restriction, including without limitation the rights
- *   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- *   copies of the Software, and to permit persons to whom the Software is
- *   furnished to do so, subject to the following conditions:
- 
- *   The above copyright notice and this permission notice shall be included in all
- *   copies or substantial portions of the Software.
- 
- *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- *   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- *   SOFTWARE.
- */
-
 /******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
@@ -9733,7 +9710,6 @@ const core_1 = __nccwpck_require__(2186);
 const github_1 = __nccwpck_require__(5438);
 try {
     const ref = github_1.context.ref;
-    console.log(github_1.context, ref);
     const branchName = ref.split("/")[ref.split("/").length - 1];
     console.log(`Running branch naming check for name ${branchName}`);
     var standard_branch = new RegExp((0, core_1.getInput)("standard_branch_check"));
@@ -9752,7 +9728,7 @@ catch (error) {
 }
 /**
  * Check wether the passed in string should be ignored as per the specifications in branching.yml
- * @param String bn
+ * @param bn
  * @returns Boolean
  */
 function checkIgnored(bn) {
