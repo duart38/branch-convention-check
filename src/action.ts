87abd115e1d463ca9744b0bb8f021020030a5722
duart38 <duartsnel@icloud.com>
2022-09-30 17:04:28 +0200

    add action.ts
    Signed-off-by: duart38 <duartsnel@icloud.com>

---
+++ src/action.ts
@@ -0,0 +1,60 @@
+/*
+ *   Copyright (c) 2022 Duart Snel
+ *   All rights reserved.
+
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy
+ *   of this software and associated documentation files (the "Software"), to deal
+ *   in the Software without restriction, including without limitation the rights
+ *   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *   copies of the Software, and to permit persons to whom the Software is
+ *   furnished to do so, subject to the following conditions:
+ 
+ *   The above copyright notice and this permission notice shall be included in all
+ *   copies or substantial portions of the Software.
+ 
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *   SOFTWARE.
+ */
+import { getInput, info, setFailed } from '@actions/core';
+import { context } from '@actions/github';
+
+try {
+  const ref = context.ref;
+  console.log(context, ref)
+  const branchName = ref.split("/")[ref.split("/").length - 1];
+  console.log(`Running branch naming check for name ${branchName}`);
+
+  var standard_branch = new RegExp(getInput("standard_branch_check"));
+
+  if (standard_branch.test(branchName)) {
+    info("Branch naming check passed.");
+  } else {
+    var caught = checkIgnored(branchName);
+    // nothing was caught.. fail..
+    if (caught === false) setFailed("Please make sure your branch complies with our naming conventions.");
+  }
+} catch (error: any) {
+  setFailed(error.message);
+}
+
+/**
+ * Check wether the passed in string should be ignored as per the specifications in branching.yml
+ * @param String bn 
+ * @returns Boolean
+ */
+function checkIgnored(bn: string){
+  var toIgnore = getInput("ignore").split(",");
+    var caught = false; // holds a boolean to dictate if this branch was ignored or not.
+    toIgnore.forEach(element => {
+      if (bn === element) {
+        info(`Ignoring branch ${element} as per specification`);
+        caught = true; // break when found
+      }
+    });
+    return caught;
+}
